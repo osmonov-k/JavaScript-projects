fb04dcd0b441325ec7c7973377a839f2cba74f44
Kanat <kanat.osmonov1@gmail.com>
2024-03-30 15:59:43 -0700

    lorem inpsum multiple articles show case

--- 13-lorem-ipsum/setup/app.js
+++ 13-lorem-ipsum/setup/app.js
@@ -19,12 +19,18 @@ const result = document.querySelector('.lorem-text');
 form.addEventListener('submit', e => {
   e.preventDefault();
   const value = parseInt(amount.value);
-  
+  const random = Math.floor(Math.random() * text.length);
   //empty
   // -1
   // >9
-  if (isNaN(value) || value < 0 || value > 9 ) {
-    result.innerHTML = `<p class="result">${text[0]}</p>`;
+  if (isNaN(value) || value <= 0 || value > 9 ) {
+    result.innerHTML = `<p class="result">${text[random]}</p>`;
+  } else {
+    let tempText = text.slice(0, value);
+    tempText = tempText.map( item => {
+      return `<p class"result">${item}</p>`;
+    }).join("");
+    result.innerHTML = `<p class="result">${tempText}</p>`;
   }
 })
 
