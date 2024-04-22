ece97489ac6ea6364c63b99d82d262fbaa433cc9
Kanat <kanat.osmonov1@gmail.com>
2024-04-21 19:17:43 -0700

    count proj finalize

--- 18-numbers/starter/app.js
+++ 18-numbers/starter/app.js
@@ -1,1 +1,25 @@
-console.log('numbers project');
+const incrementText = element => {
+    let currentValue = parseInt(element.textContent);
+    const targetValue = parseInt(element.getAttribute('data-value')); 
+    const incrementAmount = Math.ceil(targetValue / 1000);;
+
+
+    const increaseCount = setInterval( ()=> {
+        currentValue += incrementAmount;
+        if (currentValue> targetValue) {
+            element.textContent = `${targetValue}+`;
+            clearInterval(increaseCount);
+            return;
+        }
+        element.textContent = `${currentValue}+`;
+    }, 1);
+
+
+}
+
+const items = [...document.querySelectorAll('.number')];
+items.forEach(num => {
+    incrementText(num);
+})
+
+//const dataValue = spanElement.getAttribute('data-value');
