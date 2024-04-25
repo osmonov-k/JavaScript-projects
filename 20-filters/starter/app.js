7d1a7c06765acfd0141c52cb22be45d4a5dfaf6a
Kanat <kanat.osmonov1@gmail.com>
2024-04-25 16:59:43 -0700

    filters proj full functionality

--- 20-filters/starter/app.js
+++ 20-filters/starter/app.js
@@ -1,64 +1,68 @@
-const filterInput = document.querySelector('.search-input');
-const companiesContainer = document.querySelector('.companies');
+let filteredProducts = [...products];
 const productContainer = document.querySelector('.products-container');
 
-const companySet = new Set();
-products.forEach(product => {
-    companySet.add(product.company);
-});
-
-const displayFilters =  () => {
-    const companySet = new Set();
-    products.forEach(product => {
-        companySet.add(product.company);
-    });
-    const companiesArray = [...companySet]; 
-    companiesArray.unshift("all");
-    companiesContainer.innerHTML = companiesArray.map( comp => {
-        return `<button class="company-btn">${comp}</button>`;
-    }).join('');
-};
-displayFilters();
-
-const displayProducts = (filter) => { 
-    productContainer.innerHTML = products.map( product => {
-    const {title, company, image, price} = product;
-    if (filter !=  null && filter === company) {
-        return `  
-    <article class="product">
-        <img
-        src="${image}" class="product-img img" alt=""/>
-        <footer>
-            <h5 class="product-name">${title}</h5>
-            <span class="product-price">$${price}</span>
-        </footer>
-    </article>`
-    } else {
-        return `  
-    <article class="product">
-        <img
-        src="${image}" class="product-img img" alt=""/>
-        <footer>
-            <h5 class="product-name">${title}</h5>
-            <span class="product-price">$${price}</span>
-        </footer>
-    </article>`
+const displayProducts = () => { 
+    if(filteredProducts.length < 1) {
+        productContainer.innerHTML = `<h6>Sorry, no products matched your search</h6>`;
+        return;
     }
+    productContainer.innerHTML = filteredProducts.map( product => {
+    const {title, company, image, price} = product;
+    return `<article class="product">
+        <img
+        src="${image}" class="product-img img" alt=""/>
+        <footer>
+            <h5 class="product-name">${title}</h5>
+            <span class="product-price">$${price}</span>
+        </footer>
+    </article>`
     
     }).join('');
 }
 displayProducts();
 
 
-const filterBtns = document.querySelectorAll('.company-btn');
-filterBtns.forEach( btn => {
-    btn.addEventListener('click', (event)=> {
-        const textContent = event.target.textContent;
-        console.log(textContent);
-        displayProducts(textContent);
 
-    })
-})
+// text filter
+const form = document.querySelector('.input-form');
+const searchInput = document.querySelector('.search-input');
+
+searchInput.addEventListener("keypress", (event)=> {
+    const searchText = searchInput.value.trim();
+    filteredProducts = products.filter( (product)=> {
+        return product.title.toLocaleLowerCase().includes(searchText);
+    });
+    displayProducts();
+});
+
+
+const companiesContainer = document.querySelector('.companies');
+
+const displayFilterButtons =  () => {
+    const buttons = ["all", ...new Set(products.map( (product) => product.company)), ];
+    companiesContainer.innerHTML = buttons.map( (company) => {
+        return `<button class="company-btn"  data-id="${company}">${company}</button>`;
+    }).join('');
+};
+displayFilterButtons();
+
+
+companiesContainer.addEventListener('click', (event)=> {
+    const el = event.target;
+    if (el.classList.contains('company-btn')) {
+        if(el.dataset.id === 'all') {
+            filteredProducts = [...products];
+        } else {
+            filteredProducts = products.filter( (product) => {
+                return product.company === el.dataset.id;
+            });
+        };
+        searchInput.value = '';
+        displayProducts();
+    }
+});
+
+
 
 
 
