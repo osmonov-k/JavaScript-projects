ceb641aa602b4e407f4946396206074800fac475
Kanat <kanat.osmonov1@gmail.com>
2024-04-25 12:12:53 -0700

    filter proj products display funct

--- 20-filters/starter/app.js
+++ 20-filters/starter/app.js
@@ -1,1 +1,66 @@
-console.log('filters project');
+const filterInput = document.querySelector('.search-input');
+const companiesContainer = document.querySelector('.companies');
+const productContainer = document.querySelector('.products-container');
+
+const companySet = new Set();
+products.forEach(product => {
+    companySet.add(product.company);
+});
+
+const displayFilters =  () => {
+    const companySet = new Set();
+    products.forEach(product => {
+        companySet.add(product.company);
+    });
+    const companiesArray = [...companySet]; 
+    companiesArray.unshift("all");
+    companiesContainer.innerHTML = companiesArray.map( comp => {
+        return `<button class="company-btn">${comp}</button>`;
+    }).join('');
+};
+displayFilters();
+
+const displayProducts = (filter) => { 
+    productContainer.innerHTML = products.map( product => {
+    const {title, company, image, price} = product;
+    if (filter !=  null && filter === company) {
+        return `  
+    <article class="product">
+        <img
+        src="${image}" class="product-img img" alt=""/>
+        <footer>
+            <h5 class="product-name">${title}</h5>
+            <span class="product-price">$${price}</span>
+        </footer>
+    </article>`
+    } else {
+        return `  
+    <article class="product">
+        <img
+        src="${image}" class="product-img img" alt=""/>
+        <footer>
+            <h5 class="product-name">${title}</h5>
+            <span class="product-price">$${price}</span>
+        </footer>
+    </article>`
+    }
+    
+    }).join('');
+}
+displayProducts();
+
+
+const filterBtns = document.querySelectorAll('.company-btn');
+filterBtns.forEach( btn => {
+    btn.addEventListener('click', (event)=> {
+        const textContent = event.target.textContent;
+        console.log(textContent);
+        displayProducts(textContent);
+
+    })
+})
+
+
+
+
+
